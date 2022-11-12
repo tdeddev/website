52e71e0640bf7aace5507d67a62ebee237806967
tdeddev <tded.yt@gmail.com>
2022-11-12 18:38:33 +0700

    add file to project

---
+++ script.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector('.link');
+let btnfb = document.querySelector('.linkfb');
+function link(){
+    location.href = "https://www.facebook.com/SouthSeaTourNST";
+}
+
+btn.addEventListener('click', link)
+btnfb.addEventListener('click', link)
